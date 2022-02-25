d0f12b545a701600ad627749e524643b158458bd
Prajwalgn-07 <prajwalg@testvagrant.com>
2022-02-25 18:13:44 +0530

    AppiumTimeCapabilities datatype fix ------------------------------------ * changed logic in MobileTestFeed to handle the datatype * added newCommandTimeout property in desired capabilities * enum Class for AppiumTimeCapabilities

---
+++ src/main/java/com/testvagrant/ekam/mobile/AppiumTimeCapabilities.java
@@ -0,0 +1,17 @@
+package com.testvagrant.ekam.mobile;
+
+public enum AppiumTimeCapabilities {
+    newCommandTimeout,
+    appWaitDuration,
+    deviceReadyTimeout,
+    androidDeviceReadyTimeout,
+    androidInstallTimeout,
+    avdLaunchTimeout,
+    avdReadyTimeout,
+    autoWebviewTimeout,
+    adbExecTimeout,
+    launchTimeout,
+    interKeyDelay,
+    screenshotWaitTimeout,
+    webkitResponseTimeout;
+}
