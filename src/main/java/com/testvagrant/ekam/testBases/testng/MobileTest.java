e9bd879cc42ea9aa6ca4872dd21055f711b9dda7
Prajwalgn-07 <prajwalg@testvagrant.com>
2023-09-02 14:39:36 +0530

    refactored 1. Changed AppiumDriver<MobileElement> -> WebDriver 2. Changed capabilities.getVersion()-> capabilities.getBrowserVersion()

--- src/main/java/com/testvagrant/ekam/testBases/testng/MobileTest.java
+++ src/main/java/com/testvagrant/ekam/testBases/testng/MobileTest.java
@@ -4,9 +4,7 @@ import com.testvagrant.ekam.internal.executiontimeline.models.EkamTest;
 import com.testvagrant.ekam.internal.injectors.EkamMobileInjector;
 import com.testvagrant.ekam.mobile.DeviceCacheDisposeFactory;
 import com.testvagrant.ekam.mobile.models.MobileDriverDetails;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import org.slf4j.MDC;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,54 +12,55 @@ import org.testng.annotations.BeforeMethod;
 import java.util.Objects;
 
 import static com.testvagrant.ekam.internal.injectors.InjectorsCacheProvider.injectorsCache;
-import static com.testvagrant.ekam.logger.EkamLogger.ekamLogger;
 
-/** TestBase for Ekam Mobile Tests */
+/**
+ * TestBase for Ekam Mobile Tests
+ */
 public class MobileTest extends TestNgTest {
 
-  public MobileTest() {
-    super("mobile");
-  }
+    public MobileTest() {
+        super("mobile");
+    }
 
-  public MobileTest(String mobileFeed) {
-    this();
-    System.setProperty("mobileFeed", mobileFeed);
-  }
+    public MobileTest(String mobileFeed) {
+        this();
+        System.setProperty("mobileFeed", mobileFeed);
+    }
 
-  /**
-   * Executes before every test. Creates Mobile injector binding AppiumDriver, SwitchViewModule,
-   * StepRecorderModule and APIModule
-   *
-   * @param iTestResult: TestNg ITestResult
-   */
-  @BeforeMethod(alwaysRun = true)
-  public void ekamMobileSetup(ITestResult iTestResult) {
-    initLogger(iTestResult);
-    EkamTest ekamTest = buildEkamTest(iTestResult);
-    new EkamMobileInjector(ekamTest, ekamConfig).create();
-  }
+    /**
+     * Executes before every test. Creates Mobile injector binding AppiumDriver, SwitchViewModule,
+     * StepRecorderModule and APIModule
+     *
+     * @param iTestResult: TestNg ITestResult
+     */
+    @BeforeMethod(alwaysRun = true)
+    public void ekamMobileSetup(ITestResult iTestResult) {
+        initLogger(iTestResult);
+        EkamTest ekamTest = buildEkamTest(iTestResult);
+        new EkamMobileInjector(ekamTest, ekamConfig).create();
+    }
 
-  /**
-   * Executes everytime after completion of a test. Updates dashboard build with Test details
-   * Performs Appium Teardown
-   *
-   * @param iTestResult: TestNg ITestResult
-   */
-  @AfterMethod(alwaysRun = true)
-  public void ekamMobileTearDown(ITestResult iTestResult) {
-    updateBuild(iTestResult);
-    dispose();
-  }
+    /**
+     * Executes everytime after completion of a test. Updates dashboard build with Test details
+     * Performs Appium Teardown
+     *
+     * @param iTestResult: TestNg ITestResult
+     */
+    @AfterMethod(alwaysRun = true)
+    public void ekamMobileTearDown(ITestResult iTestResult) {
+        updateBuild(iTestResult);
+        dispose();
+    }
 
-  private void dispose() {
-    MobileDriverDetails mobileDriverDetails =
-        injectorsCache().getInjector().getInstance(MobileDriverDetails.class);
-    AppiumDriver<MobileElement> driver = mobileDriverDetails.getDriver();
+    private void dispose() {
+        MobileDriverDetails mobileDriverDetails =
+                injectorsCache().getInjector().getInstance(MobileDriverDetails.class);
+        WebDriver driver = mobileDriverDetails.getDriver();
 
-    if (driver != null) driver.quit();
-    if (mobileDriverDetails.getService() != null) mobileDriverDetails.getService().stop();
+        if (driver != null) driver.quit();
+        if (mobileDriverDetails.getService() != null) mobileDriverDetails.getService().stop();
 
-    DeviceCacheDisposeFactory.dispose(
-        Objects.requireNonNull(mobileDriverDetails).getTargetDetails(), ekamConfig.getMobile());
-  }
+        DeviceCacheDisposeFactory.dispose(
+                Objects.requireNonNull(mobileDriverDetails).getTargetDetails(), ekamConfig.getMobile());
+    }
 }
