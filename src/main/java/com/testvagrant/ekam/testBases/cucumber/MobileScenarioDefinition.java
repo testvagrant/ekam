b32c8449a8525db52410b5d2f5ccfbfe20cd09b1
Prajwalgn-07 <prajwalg@testvagrant.com>
2023-09-02 14:39:36 +0530

    refactored 1. Changed AppiumDriver<MobileElement> -> WebDriver 2. Changed capabilities.getVersion()-> capabilities.getBrowserVersion() 3. Reverted Url Builder changes

--- src/main/java/com/testvagrant/ekam/testBases/cucumber/MobileScenarioDefinition.java
+++ src/main/java/com/testvagrant/ekam/testBases/cucumber/MobileScenarioDefinition.java
@@ -5,9 +5,8 @@ import com.testvagrant.ekam.internal.executiontimeline.models.EkamTest;
 import com.testvagrant.ekam.internal.injectors.EkamMobileInjector;
 import com.testvagrant.ekam.mobile.DeviceCacheDisposeFactory;
 import com.testvagrant.ekam.mobile.models.MobileDriverDetails;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.WebDriver;
 
 import java.util.Objects;
 
@@ -15,22 +14,22 @@ import static com.testvagrant.ekam.internal.injectors.InjectorsCacheProvider.inj
 
 public class MobileScenarioDefinition extends ScenarioDefinition {
 
-  public void setup(Scenario scenario) {
-    initLogger(scenario);
-    EkamTest ekamTest = buildEkamTest(scenario);
-    new EkamMobileInjector(ekamTest, ekamConfig).create();
-  }
+    public void setup(Scenario scenario) {
+        initLogger(scenario);
+        EkamTest ekamTest = buildEkamTest(scenario);
+        new EkamMobileInjector(ekamTest, ekamConfig).create();
+    }
 
-  public void tearDown() {
-    MobileDriverDetails mobileDriverDetails =
-        injectorsCache().getInjector().getInstance(MobileDriverDetails.class);
-    AppiumDriver<MobileElement> driver = mobileDriverDetails.getDriver();
+    public void tearDown() {
+        MobileDriverDetails mobileDriverDetails =
+                injectorsCache().getInjector().getInstance(MobileDriverDetails.class);
+        WebDriver driver = mobileDriverDetails.getDriver();
 
-    if (driver != null) driver.quit();
-    if (mobileDriverDetails.getService() != null) mobileDriverDetails.getService().stop();
+        if (driver != null) driver.quit();
+        if (mobileDriverDetails.getService() != null) mobileDriverDetails.getService().stop();
 
-    DeviceCacheDisposeFactory.dispose(
-        Objects.requireNonNull(mobileDriverDetails).getTargetDetails(),
-        injectorsCache().getInjector().getInstance(EkamConfig.class).getMobile());
-  }
+        DeviceCacheDisposeFactory.dispose(
+                Objects.requireNonNull(mobileDriverDetails).getTargetDetails(),
+                injectorsCache().getInjector().getInstance(EkamConfig.class).getMobile());
+    }
 }
